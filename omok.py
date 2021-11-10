c872330a5b7632a9783c3e554d33eea8983508fa
jchun0829 <93302203+jchun0829@users.noreply.github.com>
2021-11-10 21:31:48 +0900

    11-10

--- omok.py
+++ omok.py
@@ -1,3 +1,8 @@
+import os
+isFinished = False
+b_win = 0
+w_win = 0
+turn = 0
 omok = [['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
         ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
         ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
@@ -12,30 +17,56 @@ omok = [['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
         ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
         ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
         ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+        ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
         ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0']]
+
+def reset_board():
+    for x in range(15):
+        for y in range(15):
+            omok[x][y] = '0'
+    global turn
+    turn = 1
 def print_board():#보드 출력
+    os.system('cls')
     for x in range(15):
         for y in range(15):
             print(omok[x][y],'',end='')
         print('')
-def depend_turn(turn):#흰돌 검돌 판단후 함수 작동
-    if turn % 2 == 0 :
-        a = 1
-    else : a = 2
-    return a
-def judge_win(po,la,di,score):#승리판단 관리
-    p=judge_win_left(po,la,di,score)+judge_win_right(po,la,di,score)
-    if( p >= 5 ) : return
+def judge_win(po,la,di,score,color):#승리판단 관리
+    if ( di == 5 ) : return 0
+    p = judge_win_left(po,la,di,score,color)+judge_win_right(po,la,di,score,color) - 1 
+    if( p >= 5 ) :
+        global b_win
+        global isFinished
+        b_win += 1
+        temp = ''
+        if color == 1:
+            temp = '검은색'
+        else:
+            temp = '흰색'
+        temp += ' 승리 다시 한번더? Y or N '
+        asw = input(temp)
+        if asw == 'Y':
+            reset_board()
+            return 0
+        else :
+            isFinished = True
+            return 0
     else :
-        judge_win(po,la,di+1,score)
-        return 
-def judge_win_right(po,la,di,score):#승리 판단 오른쪽 
+        judge_win(po,la,di+1,score,color)
+        return 0
+def judge_win_right(po,la,di,score,color):#승리 판단 오른쪽
+    if color == 1 :
+        Var = 'B'
+    else :
+        Var = 'W'
     if di ==  1 :
+
 
         if la == 15 or la < 0 and po == 15 or po < 0 :
             return score
-        elif omok[po][la] == 1 :
-            score += judge_win_right(po, la+1, di, 1)
+        elif omok[po][la] == Var :
+            score += judge_win_right(po, la+1, di, 1,color)
             return score
         else :
             return score
@@ -43,8 +74,8 @@ def judge_win_right(po,la,di,score):#승리 판단 오른쪽
 
         if la == 15 or la < 0 and po == 15 or po < 0 :
             return score
-        elif omok[po][la] == 1 :
-            score += judge_win_right(po+1, la+1, di ,1)
+        elif omok[po][la] == Var :
+            score += judge_win_right(po+1, la+1, di ,1, color)
             return score
         else :
             return score
@@ -52,8 +83,8 @@ def judge_win_right(po,la,di,score):#승리 판단 오른쪽
 
         if la == 15 or la < 0 and po == 15 or po < 0 :
             return score
-        elif omok[po][la] == 1 :
-            score += judge_win_right(po+1, la, di, 1)
+        elif omok[po][la] == Var :
+            score += judge_win_right(po+1, la, di, 1, color)
             return score
         else :
             return score 
@@ -62,18 +93,22 @@ def judge_win_right(po,la,di,score):#승리 판단 오른쪽
 
         if la == 15 or la < 0 and po == 15 or po < 0 :
             return score
-        elif omok[po][la] == 1:
-            score += judge_win(po-1,la+1,di,1)
+        elif omok[po][la] == Var:
+            score += judge_win(po-1,la+1,di,1, color)
             return score
         else :
             return score
-def judge_win_left(po,la,di,score):#승리판단 왼쪽
+def judge_win_left(po,la,di,score,color):#승리판단 왼쪽
+    if color == 1 :
+        Var = 'B'
+    else :
+        Var = 'W'
     if di == 1 :
 
         if la == 15 or la < 0 and po == 15 or po < 0 :
             return score
-        elif omok[po][la] == 1 :
-            score += judge_win_left(po,la-1,di,1)
+        elif omok[po][la] == Var :
+            score += judge_win_left(po,la-1,di,1, color)
             return score
         else :
             return score
@@ -81,8 +116,8 @@ def judge_win_left(po,la,di,score):#승리판단 왼쪽
 
         if la == 15 or la < 0 and po == 15 or po < 0 :
             return score
-        elif omok[po][la] == 1 :
-            score += judge_win_left(po-1,la-1,di,1)
+        elif omok[po][la] == Var :
+            score += judge_win_left(po-1,la-1,di,1, color)
             return score
         else :
             return score
@@ -90,8 +125,8 @@ def judge_win_left(po,la,di,score):#승리판단 왼쪽
 
         if la == 15 or la < 0 and po == 15 or po < 0 :
             return score
-        elif omok[po][la] == 1 :
-            score += judge_win_left(po-1,la,di,1)
+        elif omok[po][la] == Var :
+            score += judge_win_left(po-1,la,di,1, color)
             return score
         else :
             return score
@@ -99,32 +134,36 @@ def judge_win_left(po,la,di,score):#승리판단 왼쪽
 
         if la == 15 or la < 0 and po == 15 or po < 0 :
             return score
-        elif omok[po][la] == 1:
-            score += judge_win_left(po+1,la-1,di,1)
+        elif omok[po][la] == Var :
+            score += judge_win_left(po+1,la-1,di,1,color)
             return score
         else :
             return score
         
-turn = 0;
-print_board()
-while True:#수를 받음 
+
+while isFinished == False:#수를 받음
+    print_board()
     if( turn % 2 == 0 ):
         po = int(input('검은색돌의 세로줄을 입력하세요'))
         la = int(input('검은색돌의 가로줄을 입력하세요'))
         if( po >= 15 or po < 0 ):continue
-        if( la >= 15 or po < 0 ):continue 
+        if( la >= 15 or la < 0 ):continue 
         if( omok[po][la] == '0' ):
-            omok[po][la] = 1
+            omok[po][la] = 'B'
         else : continue
-        judge_win(po,la,1,0)
+        judge_win(po,la,1,0,1)
         
-    if( turn % 2 == 1 ):
-        po = int(input('white turn po'))
-        la = int(input('white turn la'))
+        
+    else :
+        po = int(input('흰색돌의 세로줄을 입력하세요'))
+        la = int(input('흰색돌의 가로줄을 입력하세요'))
         if( po >= 15 or po < 0 ):continue
-        if( la >= 15 or po < 0 ):continue 
+        if( la >= 15 or la < 0 ):continue 
         if( omok[po][la] == '0' ):
-            omok[po][la] = 2
+            omok[po][la] = 'W'
         else : continue
-    turn += 1
-    print_board()
+        judge_win(po,la,1,0,2)
+    turn += 1 
+    
+print('검은색 승리 횟수 :',b_win,'흰색 승리 횟수 :',w_win)
+print('프로그램을 종료합니다')
