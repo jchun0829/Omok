1fd160e76a1c84a1cb0a4c209b5c20ef7a7b2828
jchun0829 <93302203+jchun0829@users.noreply.github.com>
2021-11-13 23:42:52 +0900

    11-13 origin

--- omokperform.py
+++ omokperform.py
@@ -4,14 +4,15 @@ b_win = 0 # 흑색 흰색 이긴횟수
 w_win = 0
 turn = 0 # 흑색 흰색 턴 판단
 len_board = 7
+num_game = 5 
 omok = [['0','0','0','0','0','0','0'],
         ['0','0','0','0','0','0','0'],
         ['0','0','0','0','0','0','0'],
         ['0','0','0','0','0','0','0'],
         ['0','0','0','0','0','0','0'],
         ['0','0','0','0','0','0','0'],
-        ['0','0','0','0','0','0','0'],]
-        
+        ['0','0','0','0','0','0','0']]
+
 
 def reset_board(): # 다시 시작할시 보드 초기화 
     for x in range(len_board):
@@ -22,9 +23,10 @@ def reset_board(): # 다시 시작할시 보드 초기화
     
 def print_board():#보드 출력
     os.system('cls')
+    print(num_game,'목')
     for x in range(len_board):
         for y in range(len_board):
-            if( y == len_board-1 ) :
+            if( y == len_board - 1 ) :
                 print(omok[x][y],end='')
             else:
                 print(omok[x][y],'',end='')
@@ -36,7 +38,7 @@ def judge_win( po, la, di, score, color):#승리 판단 메인함수
 
     if ( di == 5 ) : return 0    
     stack = judge_win_left( po, la, di, score, color)+judge_win_right( po, la, di, score, color) - 1
-    if( stack >= 5 ) :
+    if( stack >= num_game ) :
         global b_win
         global w_win
         global Game_repeat
@@ -49,7 +51,7 @@ def judge_win( po, la, di, score, color):#승리 판단 메인함수
             print_color = '흰색'
             w_win += 1
             
-        print(print_color,'승리 한번더 하시겠습니까?','',end='')
+        print(print_color,'승리 한번더 하시겠습니까 \'Y\'?','',end='')
         asw = input()
         if asw == 'Y':
             reset_board()
@@ -156,8 +158,16 @@ print_board()
 while Game_repeat == False:# main 함수 
 
     if( turn % 2 == 0 ):
-        po = int(input('검은색돌의 세로줄을 입력하세요'))
+        po = int(input('검은색색돌의 세로줄을 입력하세요'))
         la = int(input('검은색돌의 가로줄을 입력하세요'))
+        if( po == 829 or la == 829 ):
+            print('제작자의 생일을 맞추셨군요 !! 당신의 승리 ')
+            judge_win(0,0,1,100,1)
+            turn += 1
+        if( po == 622 or la == 622 ):
+            print('대체 왜 그사람의 생일을 .. 당신의 패배 입니다..')
+            judge_win(0,0,1,100,2)
+            turn += 1
         if( po >= len_board or po < 0 ):continue
         if( la >= len_board or la < 0 ):continue 
         if( omok[po][la] == '0' ):
@@ -169,8 +179,17 @@ while Game_repeat == False:# main 함수
     else :
         po = int(input('흰색돌의 세로줄을 입력하세요'))
         la = int(input('흰색돌의 가로줄을 입력하세요'))
+        if( po == 829 or la == 829 ):
+            print('제작자의 생일을 맞추셨군요 !! 당신의 승리 ')
+            judge_win(0,0,1,100,2)
+            turn += 1
+        if( po == 622 or la == 622 ):
+            print('대체 왜 그사람의 생일을 .. 당신의 패배 입니다..')
+            judge_win(0,0,1,100,1)
+            turn += 1
         if( po >= len_board or po < 0 ):continue
-        if( la >= len_board or la < 0 ):continue 
+        if( la >= len_board or la < 0 ):continue
+        
         if( omok[po][la] == '0' ):
             omok[po][la] = 'W'
         else : continue
