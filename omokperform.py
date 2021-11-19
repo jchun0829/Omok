9f348aa46b6515a987c26bab00ccfc7b48f57f79
jchun0829 <93302203+jchun0829@users.noreply.github.com>
2021-11-19 23:59:43 +0900

    Update omokperform.py

--- omokperform.py
+++ omokperform.py
@@ -4,18 +4,64 @@ b_win = 0 # 흑색 흰색 이긴횟수
 w_win = 0
 turn = 0 # 흑색 흰색 턴 판단
 len_board = 7
-num_game = 5
-omok = [['0','0','0','0','0','0','0'],
-        ['0','0','0','0','0','0','0'],
-        ['0','0','0','0','0','0','0'],
-        ['0','0','0','0','0','0','0'],
-        ['0','0','0','0','0','0','0'],
-        ['0','0','0','0','0','0','0'],
-        ['0','0','0','0','0','0','0']]
+num_game = 4
+omok = [['0']*len_board for i in range(len_board)]
 Eng_Num = [[1,2,3,4,5,6,7,8,9,10,11,12,13,14],
            ['a','b','c','d','e','f','g','h','i','j','k','m','n','o'],
            ['A','B','C','D','E','F','G','H','I','J','K','M','N','O']]
 
+def repeat_game():#게임의 반복 여부
+    print('한 판 더 하시겠습니까? : ',end='')
+    user_re = input()
+    if user_re == 'Y' or user_re == 'y' :
+        return True
+    elif user_re == 'N' or user_re == 'n' :
+        return False
+    else :
+        repeat_game
+
+def game_ready():#게임이 시작시 오목판 초기화 
+    global len_board
+    global omok
+    print('오목판의 크기를 정해주세요 7~14 :',end='')
+    len_board = input()
+    try:
+        int(len_board)
+        lenboard_is = True
+    except ValueError:
+        lenboard_is = False
+    if lenboard_is == True :
+        len_board = int(len_board)
+        if len_board >= 7 and len_board <= 14 :
+            omok = [['0']*len_board for i in range(len_board)]
+            return
+        else :
+            game_ready()
+    else :
+        game_ready()
+            
+def reset_board():# 다시 시작할시 보드 초기화 
+    for x in range(len_board):
+        for y in range(len_board):
+            omok[x][y] = '0'
+    global turn
+    turn = -1
+    game_ready()    
+
+def Easter_egg(po,la,color):#특수 입력값 상호작용
+    if color == 1 :
+        other = 2
+    else :
+        other = 1
+    if po == '0829' or la == '0829' :
+        print('제작자의 생일!! 당신의 승리!!')
+        judge_win(1,1,1,100,color)
+        return True
+    if po == '0622' or la == '0622' :
+        print('당신의 패배입니다..')
+        judge_win(1,1,1,100,other)
+        return True
+    return False
 
 def po_la_input(color):#사용자 입력값 
     if( color == 1 ):
@@ -28,6 +74,8 @@ def po_la_input(color):#사용자 입력값
     po = input()
     print(VAr,'색돌의 가로줄을 입력하세요',end='')
     la = input()
+    if Easter_egg(po,la,color) == True :
+        return 0
     for x in range(1,3):
         for y in range(len_board):
             if ( la == Eng_Num[x][y] ):
@@ -60,15 +108,6 @@ def po_la_input(color):#사용자 입력값
         
     else :
         po_la_input(color)
-        
-    
-    
-def reset_board(): # 다시 시작할시 보드 초기화 
-    for x in range(len_board):
-        for y in range(len_board):
-            omok[x][y] = '0'
-    global turn
-    turn = 1
     
 def print_board(color):#보드 출력
     os.system('cls')
@@ -77,7 +116,14 @@ def print_board(color):#보드 출력
     else :
         VAR = '흰색'
     print('오목',VAR,'차례')
-    print('1 2 3 4 5 6 7')
+    for x in range(len_board):
+        if( x >= 10 ):
+            x -= 10 
+            print(x+1,'',end='')
+        else :
+            print(x+1,'',end='')
+    print()
+        
     for x in range(len_board):
         for y in range(len_board):
             if( y == len_board - 1 ):
@@ -94,17 +140,25 @@ def print_board(color):#보드 출력
                     print(omok[x][y],'',end='')
         print('')
         
-        
-        
 def judge_win( po, la, di, score, color):#승리 판단 메인함수
-
+    global b_win
+    global w_win
+    global Game_repeat
+    if ( color == 3 ):
+        print('무승부입니다 ',end='')
+        anw = repeat_game()
+        if anw == True :
+            reset_board()
+            return 0 
+        else :
+            Game_repeat = True
+            return 0
+        
     if ( di == 5 ) : return 0
     
-    stack = judge_win_left( po, la, di, score, color)+judge_win_right( po, la, di, score, color) - 1
+    stack = judge_win_left( po, la, di, score, color) + judge_win_right( po, la, di, score, color) - 1
     if( stack >= num_game ) :
-        global b_win
-        global w_win
-        global Game_repeat
+        
         
         if color == 1:
             print_color = '검은색'
@@ -115,8 +169,8 @@ def judge_win( po, la, di, score, color):#승리 판단 메인함수
             w_win += 1
             
         print(print_color,'승리 한번더 하시겠습니까 \'Y\'?','',end='')
-        asw = input()
-        if asw == 'Y':
+        asw = repeat_game()
+        if asw == True :
             reset_board()
             return 0
         
@@ -126,9 +180,8 @@ def judge_win( po, la, di, score, color):#승리 판단 메인함수
         
     else :
         judge_win( po, la, di + 1, score, color)
-        return 0
-
-    
+        return 0        
+            
 def judge_win_right( po, la, di, score, color):#승리 판단 오른쪽
 
     if color == 1 :
@@ -216,14 +269,20 @@ def judge_win_left( po, la, di, score, color):#승리판단 왼쪽
             return score
         else :
             return score
-        
-while Game_repeat == False:# main 함수 
+
+#main 함수
+game_ready()
+while Game_repeat == False:
+    if ( turn == len_board * len_board ):
+        judge_win(1,1,1,0,3)
+        turn += 1
     print_board(turn)
     if( turn % 2 == 0 ):
         po_la_input(1)   
     else :
         po_la_input(2)  
-    turn += 1 
+    turn += 1
+    
 
 
 print('검은색 승리 횟수 :',b_win,'흰색 승리 횟수 :',w_win)
